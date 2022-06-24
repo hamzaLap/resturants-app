578269bef3a0d88f8e2950259d1e1f3574c278c3
ESHamza <essaadihamza4@outlook.com>
2022-06-24 21:23:22 +0100

    still working on laking the navbar responsive

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-black text-white font-sans flex justify-between px-5 py-4 text-lg md:text-xl">
+  <nav class="bg-black w-full text-white font-sans flex justify-between px-5 py-4 text-lg md:text-xl">
     <a href="" class="hover:text-green-600">Restaurantly</a>
    <div class="flex justify-between w-1/3 md:w-44"   >
       <a href="/" class="hover:text-green-600">Home</a>
