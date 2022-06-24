f8693d84eebd2cfa6f740b60eef5e2186342f5fc
ESHamza <essaadihamza4@outlook.com>
2022-06-24 22:48:26 +0100

    created restinfo componenet to render the restaurant that has been clicked

---
+++ components/restinfos.vue
@@ -0,0 +1,22 @@
+<template>
+<main>
+
+    <section>
+       <h1> {{the_restaurant.name}}</h1>
+       <h1>rank is {{the_restaurant.rank}}</h1>
+       <h1>number of stores {{the_restaurant.numberOfStores}}</h1>
+       <h1>revenue is {{the_restaurant.revenue}}</h1>
+     </section>
+      <div>
+        <img :src="the_restaurant.imageUrl" :alt="the_restaurant.name">
+    
+        <p>{{  this.the_restaurant.content}}</p>
+    </div>
+</main>
+</template>
+
+<script>
+export default {
+props:["the_restaurant"]
+}
+</script>
