6025014e47601ab497b717bc2720fce14218d5be
ESHamza <essaadihamza4@outlook.com>
2022-06-24 21:40:43 +0100

    create a new directory

---
+++ pages/restaurants/_names.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <NuxtLink to="/restaurants">
+    <button class="w-20  bg-green-200 m-5 rounded hover:bg-green-300" >back</button>
+  </NuxtLink>
+ <section>
+   <h1> {{rest_name}}</h1>
+   <h1>rank is {{the_restaurant.rank}}</h1>
+   <h1>number of stores {{the_restaurant.numberOfStores}}</h1>
+   <h1>revenue is {{the_restaurant.revenue}}</h1>
+ </section>
+  <div>
+    <img :src="the_restaurant.imageUrl" :alt="the_restaurant.name">
+
+    <p>{{  this.the_restaurant.content}}</p>
+
+</div>
+</div>
+</template>
+
+<script>
+import resturantsObj from "@/data.json"
+
+export default {
+  mounted(){
+    this.the_restaurant=resturantsObj.find(rest => rest.name==this.rest_name)
+  },
+data(){
+  return{
+    rest_name:this.$route.params.names,
+    the_restaurant: {}
+  }
+}
+}
+</script>
+var name =
+<style>
+
+</style>
---
+++ pages/restaurants/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <navbar />
+    <h1 class="mt-20 mb-10 mx-20 text-2xl">Restaurants page</h1>
+    <!-- section contains the table -->
+    <section class="mx-20 mb-20 flex flex-wrap    md:flex-nowrap md:space-y-0 lg:space-x-5 ">
+      <table class="w-96 table-auto border md:w-100">
+        <!-- table head -->
+         <resturantsTabhead />
+          <tbody>
+            <!-- table rows  -->
+          <ResturantsTabrow v-for="rest in first_resturants" :the_resturant="rest" :key="rest.id"     /> 
+          </tbody>
+      </table>
+       <table class="w-96 table-auto border md:w-100 ">
+        <!-- table head -->
+         <resturantsTabhead class="hidden md:block"/>
+          <!-- table rows  -->
+          <tbody>
+            <ResturantsTabrow v-for="rest in second_resturants " :the_resturant="rest" :key="rest.id" />  
+          </tbody>
+       </table>
+    </section>
+
+
+  </div>
+
+</template>
+
+<script>
+import resturants from "@/data.json"
+
+export default {
+  mounted(){
+      //  this.Resturants=JSON.parse(resturants),
+       this.Resturants=[...resturants]
+       this.first_resturants=this.Resturants.splice(0,25),
+       this.second_resturants=this.Resturants.splice(0,25)
+       console.log(this.second_resturants);
+    },
+  data(){
+   return{
+         Resturants:[],
+         first_resturants:  [] ,
+         second_resturants:[]    
+        }
+      }
+}
+</script>
+
+<style>
+
+</style>
