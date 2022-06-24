0fa001908a5c027dbb0b687a0ae4017769116df5
ESHamza <essaadihamza4@outlook.com>
2022-06-24 21:32:15 +0100

    fixed table header to be responsive

--- components/resturants/tabrow.vue
+++ components/resturants/tabrow.vue
@@ -1,10 +1,11 @@
 <template>
   <tr>
-    <td class="border p-2">{{ the_resturant.rank }}</td>
-    <td class="border p-2"><a :href="`/resturants/${the_resturant.name}`">{{ the_resturant.name }}</a></td>
+    <td class="border p-2 w-32">{{ the_resturant.rank }}</td>
+    <td class="border p-2"> <NuxtLink :to="`/restaurants/${the_resturant.name}`">{{ the_resturant.name }}</NuxtLink ></td>
   </tr>
 </template>
 
+<!-- :to="`/restaurants/${the_resturant.name}`" -->
 <script>
 export default {
   props: ["the_resturant"]
