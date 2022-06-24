df7408c3f08a4f760959af91cf5b25014ff7ac17
ESHamza <essaadihamza4@outlook.com>
2022-06-24 21:38:00 +0100

    deleted the index.vue file and palace it inside the restaurants folder

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <h1 class="mx-auto mt-20 mb-5 text-3xl font-sans md:text-4xl">Welcome to Resturantly</h1>
       </div> 
       <div  class="flex">
-        <a href="/resturants" class="mx-auto inline-block md:text-lg">go to restaurants</a>
+        <a href="/restaurants" class="mx-auto inline-block md:text-lg">go to restaurants</a>
       </div>
 </div>
 </template>
